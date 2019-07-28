2b00f4c1a44e666fb9a466a24579b5864e32ba3d
pallabghosh <mr.pallabghosh@gmail.com>
2019-07-29 00:46:22 +0530

    my tests micro 3

---
+++ netflix-zuul-api-gateway-server/src/test/java/com/pg/microservices/gateway/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
